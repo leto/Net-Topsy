8c2866d3377625df5fde8dbbc5d5c9616077e5b5
Duke Leto <jonathan@leto.net>
2009-09-01 23:53:20 -0700

    Update MANIFEST(.SKIP) and search example

--- examples/search.pl
+++ examples/search.pl
@@ -5,6 +5,23 @@ use Net::Topsy;
 use Data::Dumper;
 my $search_term = shift || 'perl';
 
+=head1 SYNOPSIS
+
+TOPSY_API_KEY=somekey perl -Ilib examples/search.pl search_term
+
+Shows the top 30 matches for today.
+
+=cut
+
 my $topsy = Net::Topsy->new( beta_key => $ENV{TOPSY_API_KEY} );
-my $search = $topsy->search( { q => $search_term, perpage => 30 } );
-warn Dumper [ $search ];
+my $search = $topsy->search({
+                               q => $search_term,
+                               page   =>  1,  # default
+                               perpage => 30, # 30 per page
+                               window => 'd', # today
+                            });
+my $topics = $search->{response}{list};
+
+for my $topic (@$topics) {
+    printf "%-60s : %d : %s\n", $topic->{title} ,$topic->{hits}, $topic->{url};
+}
