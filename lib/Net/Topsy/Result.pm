9883e7e28ed32a28f6d73cc5eac464f4218eb9cc
Duke Leto <jonathan@leto.net>
2009-09-09 23:36:07 -0700

    iterations in the result object

--- lib/Net/Topsy/Result.pm
+++ lib/Net/Topsy/Result.pm
@@ -7,8 +7,14 @@ class Net::Topsy::Result {
     has perl     => ( isa => 'HashRef', is => 'rw' );
     has json     => ( isa => 'Str',     is => 'rw' );
     has response => ( isa => 'HTTP::Response', is => 'rw' );
+    has _list    => ( isa => 'ArrayRef', is => 'rw', default => sub { [ ] } );
+    has iter => (
+        metaclass    => 'Iterable',
+        iterate_over => '_list',
+    );
 
     method BUILD {
+        $self->_list( @{$self->perl->{response}{list}} );
         return $self;
     }
 }
