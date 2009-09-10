4c71427302e74f701b93a41e49b724a261b1b4c6
Duke Leto <jonathan@leto.net>
2009-09-09 21:55:40 -0700

    Update Build.PL and add MooseX::Iterator

--- lib/Net/Topsy/Result.pm
+++ lib/Net/Topsy/Result.pm
@@ -1,6 +1,6 @@
 package Net::Topsy::Result;
 use Moose;
-
+use MooseX::Iterator;
 has perl => ( isa => 'HashRef', is => 'rw' );
 has json => ( isa => 'Str',     is => 'rw' );
 has response => ( isa => 'HTTP::Response', is => 'rw' );
